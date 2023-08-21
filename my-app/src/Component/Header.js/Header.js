f82e755262e08f96ca3d8f1492d0f185ebd56d92
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-08-22 02:26:19 +0600

    login and react hook form in shipment

--- my-app/src/Component/Header.js/Header.js
+++ my-app/src/Component/Header.js/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import alogo from '../../images/logo.png';
 import './Header.css';
 import {Link, Router } from 'react-router-dom';
+import { userLoginconstext } from '../../App';
 
 const Header = () => {
+
+    const [loginuser,setloginuser]=useContext(userLoginconstext);
     return (
         <div>
            <div className="logo">
@@ -15,13 +18,10 @@ const Header = () => {
            
                <li><Link className='customlink' to ="/">Shop </Link></li>
                <li><Link className='customlink' to ="/review"> Order Review</Link></li>
-               <li><Link className='customlink' to ="inventory">Manage Inventory</Link></li>
-               
+               <li><Link className='customlink' to ="/inventory">Manage Inventory</Link></li>
+               <li><button onClick={()=>setloginuser("")}> Signout</button></li>
                 
-                
-                
-          
- 
+        
            
            </ul>
            </div>
--- my-app/src/Component/Shipment/Shipment.js
+++ my-app/src/Component/Shipment/Shipment.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import { userLoginconstext } from '../../App';
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  const [loginuser,setloginuser]=useContext(userLoginconstext);
     return (
-        <div>
-            <h1>this is shipment page.</h1>
-        </div>
-    );
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <input defaultValue={loginuser.name} {...register("example")} />
+          <input defaultValue={loginuser.email} {...register("example1")} />
+          
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+          
+          <input type="submit" />
+        </form>
+      );
 };
 
 export default Shipment;
